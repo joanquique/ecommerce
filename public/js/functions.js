4a4c72b9c6ad06d34cbcffb9bf55b781cb6d97a4
Joan Alvarez <joanquique@gmail.com>
2024-04-25 16:50:30 -0600

    Adicion de menu/menu

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,3 @@
-console.log("Script cargado correctamente");
-
 document.getElementById('cart-toggle').addEventListener('click', function() {
     console.log("Clic en el icono de carrito");
     document.querySelector('.cart').classList.toggle('cart--open');
@@ -8,5 +6,13 @@ document.getElementById('cart-toggle').addEventListener('click', function() {
 document.getElementById('cart-close').addEventListener('click', function() {
     console.log("Clic en el icono de cerrar carrito");
     document.querySelector('.cart').classList.remove('cart--open');
+});
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('menu--open');
+});
+
+document.getElementById('menu-close').addEventListener('click', function() {
+    document.querySelector('.menu').classList.remove('menu--open');
 });
 
