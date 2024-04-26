4f9de5fb510f3e686184a932f8b138859b934312
Joan Alvarez <joanquique@gmail.com>
2024-04-25 18:14:07 -0600

    Badge agregado/carrito

--- public/js/functions.js
+++ public/js/functions.js
@@ -16,3 +16,67 @@ document.getElementById('menu-close').addEventListener('click', function() {
     document.querySelector('.menu').classList.remove('menu--open');
 });
 
+// Selecciona todos los botones "Agregar al carrito" y agrega un evento de clic a cada uno
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const product = button.parentNode;
+        const productName = product.querySelector('h3').textContent;
+        const productPrice = product.querySelector('p').textContent;
+        const imgSrc = product.querySelector('img').getAttribute('src');
+        addToCart(productName, productPrice, imgSrc);
+    });
+});
+
+// Función para agregar un producto al carrito
+function addToCart(name, price, imgSrc) {
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart__item');
+
+    // Imagen del producto
+    const img = document.createElement('img');
+    img.src = imgSrc;
+    img.alt = name;
+    cartItem.appendChild(img);
+
+    // Nombre y precio del producto
+    const itemName = document.createElement('p');
+    itemName.textContent = name;
+    cartItem.appendChild(itemName);
+
+    const itemPrice = document.createElement('p');
+    itemPrice.textContent = price;
+    cartItem.appendChild(itemPrice);
+
+    // Botón para eliminar el producto del carrito
+    const deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('cart__item-delete-icon');
+    deleteIcon.innerHTML = `<img src="img/quitar.png" alt="Icono Quitar">`;
+    deleteIcon.addEventListener('click', () => {
+        cartItem.remove(); // Elimina el elemento del carrito
+        updateCartItemCount(-1); // Disminuye el contador de productos
+    });
+    cartItem.appendChild(deleteIcon);
+
+    // Agrega el elemento del producto al carrito
+    const cart = document.querySelector('.cart');
+    cart.appendChild(cartItem);
+
+    // Aumenta el contador de productos y actualiza el badge
+    updateCartItemCount(1);
+}
+
+let cartItemCount = 0;
+
+// Actualizar el contador en el badge
+function updateCartItemCount(count) {
+    cartItemCount = parseInt(cartItemCount) + count;
+    localStorage.setItem('cartItemCount', cartItemCount);
+    updateCartBadge();
+}
+
+// Actualizar el badge del carrito con el contador actualizado
+function updateCartBadge() {
+    const cartBadge = document.querySelector('.cart__badge');
+    cartBadge.textContent = cartItemCount;
+}
